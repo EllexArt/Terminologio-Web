7015d31add3a52af764d25b13e00dbf714b2c155
mazuyaxe <axelle.mazuy@univ-rouen.fr>
2023-12-15 21:13:00 +0100

    Feature : add docker ignore

--- src/Controller/TerminologioController.php
+++ src/Controller/TerminologioController.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace App\Controller;
-use Psr\Container\ContainerExceptionInterface;
-use Psr\Container\NotFoundExceptionInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,18 +23,6 @@ class TerminologioController extends AbstractController {
 
     #[Route('/signingIn', name:'app_terminologio_signingIn')]
     public function signingIn(string $user_name, string $user_mail, string $user_passwd, string $user_passwd_confirm) : Response {
-        try {
-            $userManagementService = $this->container->get('registerUser');
-        } catch (NotFoundExceptionInterface $e) {
-            echo "Not found";
-        } catch (ContainerExceptionInterface $e) {
-            echo "Container Exception";
-        }
-        return $this->redirect('/');
-    }
-
-    #[Route('/connecting', name:'app_terminologio_connecting')]
-    public function connecting(string $user_identification, string $user_passwd) : Response {
         $userManagementService = $this->container->get('registerUser');
         return $this->redirect('index.html.twig');
     }
