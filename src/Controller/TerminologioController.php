68a087dec629c193e5639f51105453d3584046ca
mazuyaxe <axelle.mazuy@univ-rouen.fr>
2023-12-15 21:13:00 +0100

    Feature : add signing in route

--- src/Controller/TerminologioController.php
+++ src/Controller/TerminologioController.php
@@ -8,7 +8,7 @@ class TerminologioController extends AbstractController {
 
     #[Route('/', name:'app_terminologio_index')]
     public function index() : Response {
-        return $this->render('index.html.twig', []);
+        return $this->render('index.html.twig', ['controller_name' => 'TerminologioController']);
     }
 
     #[Route('/inscription', name:'app_terminologio_inscription')]
@@ -19,5 +19,11 @@ class TerminologioController extends AbstractController {
     #[Route('/connect', name:'app_terminologio_connection')]
     public function connection() : Response {
         return $this->render('connect.html.twig', []);
+    }
+
+    #[Route('/signingIn', name:'app_terminologio_signingIn')]
+    public function signingIn(string $user_name, string $user_mail, string $user_passwd, string $user_passwd_confirm) : Response {
+        $userManagementService = $this->container->get('registerUser');
+        return $this->redirect('index.html.twig');
     }
 }
