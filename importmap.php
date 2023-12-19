d05c40c4aaaf62532a88c09bddb6d0677d7443f4
bingieti <etienne.binginot@univ-rouen.fr>
2023-12-19 21:18:02 +0100

    Feature : asset mapper tailwind, add admin management

--- importmap.php
+++ importmap.php
@@ -17,5 +17,11 @@ return [
     'app' => [
         'path' => './assets/app.js',
         'entrypoint' => true,
-    ]
+    ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
+    '@symfony/stimulus-bundle' => [
+        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+    ],
 ];
