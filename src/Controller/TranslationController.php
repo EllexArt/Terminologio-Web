1f8ae9c9e2eafbae9d9ccfc58e8416837773ad0e
ellexart <axelle.mazuy@gmail.com>
2023-12-28 18:14:19 +0100

    Feature : security access when not connected

--- src/Controller/TranslationController.php
+++ src/Controller/TranslationController.php
@@ -13,7 +13,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 class TranslationController extends AbstractController
 {
     #[Route('/concept/{title}/translate', name: 'app_concept_translation')]
