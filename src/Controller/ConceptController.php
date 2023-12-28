334492c8a78cf242c24c46c489578e9f38f2f25d
ellexart <axelle.mazuy@gmail.com>
2023-12-28 18:14:19 +0100

    Feature : security access when not connected

--- src/Controller/ConceptController.php
+++ src/Controller/ConceptController.php
@@ -12,8 +12,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
+#[IsGranted('ROLE_USER')]
 class ConceptController extends AbstractController
 {
     #[Route('/concept/create', name: 'app_concept_upload')]
